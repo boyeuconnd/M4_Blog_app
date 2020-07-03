4feb7bd08fb538407501a946588ce7c74730e3bc
Dang Pham <thienvu453@gmail.com>
2020-07-03 15:11:40 +0700

    Demo Spring Security & Custom Login form

---
+++ src/main/java/cg/blog/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package cg.blog;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and().authorizeRequests().antMatchers("/blog/**").hasAnyRole("USER", "ADMIN")
+                .and().formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/custom-login")
+        ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("12345").roles("USER")
+                .and()
+                .withUser("admin").password("12345").roles("ADMIN");
+    }
+}
---
+++ src/main/java/cg/blog/SecurityWebApplicationInit.java
@@ -0,0 +1,6 @@
+package cg.blog;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInit extends AbstractSecurityWebApplicationInitializer {
+}
